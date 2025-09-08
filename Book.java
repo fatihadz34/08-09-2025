886d7e5b0da9f2d218bef9c93234f58d1d235917
fatihadz34 <fatih_adzahabi_34rpl@student.smktelkom-mlg.sch.id>
2025-09-08 14:47:01 +0700

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,43 @@
+public class Book {
+    private String title;
+    private int year;
+    private double price;
+
+    public Book() {
+        title = "Into the Abyss";
+        year = 1978;
+        price = 150000;
+    }
+
+    public Book(String title, int year, double price) {
+        this.title = title;
+        this.year = year;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Year: " + year);
+        System.out.println("Price: " + price);
+    }
+
+}
